8f316876ab150b172c408ab457661ed942ed605a
Nickil Maveli <nickil21jhum@gmail.com>
2015-12-12 23:17:12 +0530

    Exercise-1

---
+++ machine-learning-ex1/ex1/plotData.m
@@ -0,0 +1,11 @@
+function plotData(x, y)
+figure;
+
+
+ylabel('Profit in $10,000s'); % Set the y−axis label 
+xlabel('Population of City in 10,000s'); % Set the x−axis label 
+plot(x, y, 'rx', 'MarkerSize', 10); % Plot the data
+
+
+
+end
